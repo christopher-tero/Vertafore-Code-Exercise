8caa26091ba54efaae8f54fb8fee40d4773b950c
Christopher Tero <christopher.tero@gmail.com>
2019-12-23 11:08:47 -0600

    updated abbreviator, allowed for checking individual word against dictionary, cleaned up code

--- WordAbbreviator/WordAbbreviator/Program.cs
+++ WordAbbreviator/WordAbbreviator/Program.cs
@@ -6,28 +6,26 @@ namespace WordAbbreviator
 {
     public class Abbreviator
     {
+        public static string[] dictionary = { "internationalization", "localization", "accessibility", "automatically" };
+
         public static string Abbreviate(string word)
         {
             if (!Regex.IsMatch(word, @"^[a-zA-Z]+$"))
             {
-                //Console.WriteLine("Invalid entry");
                 return "Invalid entry";
             }
             else if (word.Length > 2)
             {
                 int middleLetters = word.Length - 2;
                 string abbreviation = word[0] + middleLetters.ToString() + word[word.Length - 1];
-                //Console.WriteLine(abbreviation);
                 return abbreviation;
             }
             else if (word.Length > 0)
             {
-                //Console.WriteLine(word);
                 return word;
             }
             else
             {
-                //Console.WriteLine("No word was given");
                 return "No word was given";
             }
         }
@@ -50,12 +48,10 @@ namespace WordAbbreviator
             return abbreviatedArray;
         }
 
-        public static string[] CheckArray(string[] array)
+        public static Dictionary<string, int> AbbreviateDictionary(string[] array)
         {
             string[] arrayToCheck;
-            string[] arrayUniqueness;
             arrayToCheck = AbbreviateArray(array);
-            arrayUniqueness = new string[array.Length];
 
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
 
@@ -71,27 +67,35 @@ namespace WordAbbreviator
                 }
             }
 
-            foreach (var item in dictionary)
+            return dictionary;
+        }
+
+        public static bool IsWordUnique(string word)
+        {
+            string abbreviatedWord = Abbreviator.Abbreviate(word);
+            Dictionary<string, int> abbreviatedDictionary = AbbreviateDictionary(Abbreviator.dictionary);
+
+            foreach (var item in abbreviatedDictionary)
             {
-                if (item.Value == 1)
+                if (abbreviatedWord == item.Key)
                 {
-                    Console.WriteLine(item.Key + ": true");
-                }
-                else
-                {
-                    Console.WriteLine(item.Key + " false");
+                    if (item.Value == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
             }
-
-
-
-            return arrayUniqueness;
+            return false;
         }
     }
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Abbreviator test
             Console.WriteLine(Abbreviator.Abbreviate("Hello")); // expecting H3o
@@ -100,7 +104,13 @@ namespace WordAbbreviator
             Console.WriteLine(Abbreviator.Abbreviate("H4ll0")); // expecting Invalid Entry
 
             string[] dictionary = { "internationalization", "localization", "accessibility", "automatically" };
-            CheckUniqueness.CheckArray(dictionary);
+            CheckUniqueness.AbbreviateDictionary(dictionary);
+
+            // Abbreviation uniqueness test
+            Console.WriteLine(CheckUniqueness.IsWordUnique("internationalization"));
+            Console.WriteLine(CheckUniqueness.IsWordUnique("localization"));
+            Console.WriteLine(CheckUniqueness.IsWordUnique("accessibility"));
+            Console.WriteLine(CheckUniqueness.IsWordUnique("automatically"));
         }
     }
 }
